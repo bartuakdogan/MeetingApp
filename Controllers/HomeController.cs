3222931bbcd39ea27b8f73a18b9535059a7a88e2
Bartu Akdoğan <bartuakdogan8@gmail.com>
2024-11-06 17:13:18 +0300

    MeetingApp Project by Bartu Akdogan

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MeetingApp1.Models;
+
+namespace MeetingApp1.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        int saat = DateTime.Now.Hour;
+        
+        ViewBag.Selamlama = saat > 12 ? "İyi Günler" : "Günaydın";
+        ViewBag.UserCount = Repository.Users.Where(i => i.WillAttend == true).Count();
+        // ViewBag.UserName = "Bartu";
+
+        var meetingInfo = new MeetingInfo()
+        {
+            Id = 1,
+            Location = "İstanbul, Bostancı Gösteri Merkezi",
+            Date = new DateTime(2024, 01, 20, 20, 0, 0),
+            NumberOfPeople = 100
+        };
+        
+        return View(meetingInfo);
+    }
+    
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,31 @@
+﻿namespace MeetingApp1.Models;
+
+public static class Repository
+{
+    private static List<UserInfo> _users = new();
+
+    static Repository()
+    {
+        _users.Add(new UserInfo() {Id = 1, Name = "Melisa", Email = "melisa@gmail.com", Phone = "123456798", WillAttend = true});
+        _users.Add(new UserInfo() {Id = 2, Name = "Mustafa", Email = "mustafa@gmail.com", Phone = "123456789", WillAttend = false});
+        _users.Add(new UserInfo() {Id = 3, Name = "Meral", Email = "meral@gmail.com", Phone = "213456789", WillAttend = true});
+    }
+
+    public static List<UserInfo> Users
+    {
+        get {
+            return _users;
+        }
+    }
+
+    public static void CreateUser(UserInfo user)
+    {
+        user.Id = Users.Count() + 1;
+        _users.Add(user);
+    }
+
+    public static UserInfo? GetById(int id)
+    {
+        return _users.FirstOrDefault(user => user.Id == id);
+    }
+}
